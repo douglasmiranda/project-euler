5151ca1b7ded04b97ba40a1606b26b27ab0c0d99
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-07-14 17:22:26 -0400

    Optimizing the is_palindrome function

--- python/problem_4.py
+++ python/problem_4.py
@@ -13,4 +13,4 @@ def is_palindrome(number):
 		return True
 	return is_palindrome(string[1:-1]) if string[0] == string[-1] else False
 
-print max([x*y for x in xrange(100,1000) for y in xrange(100,1000) if is_palindrome(x*y)])
+# print max([x*y for x in xrange(100,1000) for y in xrange(100,1000) if is_palindrome(x*y)])
