6c117bc0ccc060444f81b3d2f540cf960e966a75
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-07-24 22:21:00 -0400

    problems 25 and 48 solved!

---
+++ python/problem_48.py
@@ -0,0 +1,7 @@
+"""
+The series, 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317.
+
+Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
+"""
+
+print str(sum([x**x for x in xrange(1,1001)]))[-10:]
